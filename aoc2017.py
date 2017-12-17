abb6000b696a90102164e1fc937b7f0148b45252
Anders Blehr <anders@andersblehr.co>
2017-12-17 09:25:40 +0100

    Day 17

--- aoc2017.py
+++ aoc2017.py
@@ -2033,6 +2033,109 @@ def day16_2():
     remainder = 1000000000 % period
     return states[remainder]
 
+'''
+--- Day 17: Spinlock ---
+
+Suddenly, whirling in the distance, you notice what looks like a massive,
+pixelated hurricane: a deadly spinlock. This spinlock isn't just consuming
+computing power, but memory, too; vast, digital mountains are being ripped
+from the ground and consumed by the vortex.
+
+If you don't move quickly, fixing that printer will be the least of your
+problems.
+
+This spinlock's algorithm is simple but efficient, quickly consuming
+everything in its path. It starts with a circular buffer containing only
+the value 0, which it marks as the current position. It then steps forward
+through the circular buffer some number of steps (your puzzle input)
+before inserting the first new value, 1, after the value it stopped on.
+The inserted value becomes the current position. Then, it steps forward
+from there the same number of steps, and wherever it stops, inserts after
+it the second new value, 2, and uses that as the new current position
+again.
+
+It repeats this process of stepping forward, inserting a new value, and
+using the location of the inserted value as the new current position a
+total of 2017 times, inserting 2017 as its final operation, and ending
+with a total of 2018 values (including 0) in the circular buffer.
+
+For example, if the spinlock were to step 3 times per insert, the circular
+buffer would begin to evolve like this (using parentheses to mark the
+current position after each iteration of the algorithm):
+
+  - (0), the initial state before any insertions.
+  - 0 (1): the spinlock steps forward three times (0, 0, 0), and then
+    inserts the first value, 1, after it. 1 becomes the current position.
+  - 0 (2) 1: the spinlock steps forward three times (0, 1, 0), and then
+    inserts the second value, 2, after it. 2 becomes the current position.
+  - 0  2 (3) 1: the spinlock steps forward three times (1, 0, 2), and then
+    inserts the third value, 3, after it. 3 becomes the current position.
+
+And so on:
+
+  - 0  2 (4) 3  1
+  - 0 (5) 2  4  3  1
+  - 0  5  2  4  3 (6) 1
+  - 0  5 (7) 2  4  3  6  1
+  - 0  5  7  2  4  3 (8) 6  1
+  - 0 (9) 5  7  2  4  3  8  6  1
+
+Eventually, after 2017 insertions, the section of the circular buffer near
+the last insertion looks like this:
+
+1512  1134  151 (2017) 638  1513  851
+
+Perhaps, if you can identify the value that will ultimately be after the
+last value written (2017), you can short-circuit the spinlock. In this
+example, that would be 638.
+
+What is the value after 2017 in your completed circular buffer?
+
+Your puzzle answer was 1642.
+'''
+def day17_1(test_input=None):
+    steps = test_input if test_input else input_for_day(17)
+    position = 0
+    spinlock = [position]
+    value = 1
+    while value < 2018:
+        position = (position + (steps % value)) % value
+        spinlock.insert(position + 1, value)
+        position += 1
+        value += 1
+    return spinlock[position + 1]
+
+'''
+The spinlock does not short-circuit. Instead, it gets more angry. At
+least, you assume that's what happened; it's spinning significantly faster
+than it was a moment ago.
+
+You have good news and bad news.
+
+The good news is that you have improved calculations for how to stop the
+spinlock. They indicate that you actually need to identify the value after
+0 in the current state of the circular buffer.
+
+The bad news is that while you were determining this, the spinlock has
+just finished inserting its fifty millionth value (50000000).
+
+What is the value after 0 the moment 50000000 is inserted?
+
+Your puzzle answer was 33601318.
+'''
+def day17_2(test_input=None):
+    steps = test_input if test_input else input_for_day(17)
+    position = 0
+    value = 1
+    value_at_1 = 0
+    while value < 50000000:
+        position = (position + (steps % value)) % value
+        if position == 0:
+            value_at_1 = value
+        position += 1
+        value += 1
+    return value_at_1
+
 #
 # ADDITIONAL FUNCTIONS
 #
@@ -2106,6 +2209,8 @@ def input_for_day(day, puzzle=1):
         return generator_inputs
     elif day == 16:
         return input.split(',')
+    elif day == 17:
+        return int(input)
 
 '''
 Print out results for:
@@ -2132,6 +2237,7 @@ def aoc(day=None, puzzle=None):
         14: {1: day14_1, 2: day14_2},
         15: {1: day15_1, 2: day15_2},
         16: {1: day16_1, 2: day16_2},
+        17: {1: day17_1, 2: day17_2},
     }
 
     def print_result(day, puzzle):
